cf29b139c134e4a282aa1521407aedf34954948a
Samuel Gonshaw <sjg10@imperial.ac.uk>
2013-12-20 17:22:03 +0000

    Prepared for image analysis

--- src/com/example/sudokoer/CameraActivity.java
+++ src/com/example/sudokoer/CameraActivity.java
@@ -2,12 +2,27 @@ package com.example.sudokoer;
 
 import java.io.IOException;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.PixelFormat;
+import android.graphics.Bitmap.Config;
 import android.hardware.Camera;
 import android.hardware.SensorManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.KeyEvent;
 import android.view.OrientationEventListener;
@@ -24,7 +39,34 @@ import android.os.Process;
 public class CameraActivity extends Activity implements Camera.PictureCallback,SurfaceHolder.Callback{
 	public Camera camera;
 	public int mLastRotation=-1;
-	public OrientationEventListener orientationEventListener; 
+	public OrientationEventListener orientationEventListener;
+	public SurfaceView surfaceView;
+	public SurfaceHolder surfaceHolder;
+	public AsyncTask<byte[], Integer, String> ComputeSudoku;
+	//This ensures that we retrieve things from OpenCV app.
+	//Consider making static!
+	private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+	    @Override
+	    public void onManagerConnected(int status) {
+	        switch (status) {
+	            case LoaderCallbackInterface.SUCCESS:
+	            {
+	                Log.i("OpenCV", "OpenCV loaded successfully");
+	            } break;
+	            default:
+	            {
+	                super.onManagerConnected(status);
+	            } break;
+	        }
+	    }
+	};
+
+	@Override
+	public void onResume()
+	{
+	    super.onResume();
+	    OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
+	}
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -35,7 +77,7 @@ public class CameraActivity extends Activity implements Camera.PictureCallback,S
             WindowManager.LayoutParams.FLAG_FULLSCREEN);
         
 		setContentView(R.layout.activity_camera);
-		
+
 		orientationEventListener = new OrientationEventListener(this,
 		        SensorManager.SENSOR_DELAY_NORMAL) {
 		    @Override
@@ -71,11 +113,12 @@ public class CameraActivity extends Activity implements Camera.PictureCallback,S
 		Camera.Parameters parameters = camera.getParameters();
 	    parameters.setRotation(0);
 	    parameters.setZoom(0);
+	    parameters.setPictureFormat(PixelFormat.RGB_565);
 	    camera.setParameters(parameters);
 	    //this.onConfigurationChanged(this.getResources().getConfiguration());
 	    orientationEventListener.onOrientationChanged(0);
-		SurfaceView surfaceView = (SurfaceView)findViewById(R.id.surfaceView1);
-		SurfaceHolder surfaceHolder=surfaceView.getHolder();
+	    surfaceView = (SurfaceView)findViewById(R.id.surfaceView1);
+	    surfaceHolder=surfaceView.getHolder();
 		surfaceHolder.addCallback(this);
 		
 	}
@@ -83,11 +126,60 @@ public class CameraActivity extends Activity implements Camera.PictureCallback,S
 	@Override
 	public void onPictureTaken(byte[] data, Camera camera) {
 		Toast.makeText(this, "Picture taken", Toast.LENGTH_LONG).show();
-		//TODO: begin work!
-	}
+		//prepare AsyncTask to run:
+		ComputeSudoku = new AsyncTask<byte[], Integer, String>() {
+		     protected String doInBackground(byte[] data) {
+		    	//encode for OpenCV
+		 		Mat raw = new Mat(1, data.length, CvType.CV_8U); 
+		 		//TODO: Not outputting grayscale!
+		 		Mat M=Highgui.imdecode(raw, Highgui.IMREAD_GRAYSCALE);
+		 		
+		 		//apply transformations
+		 		Mat A = new Mat(M.size(),M.type());
+		 		//TODO: THRESHOLD NOT DOING ANYTHING (just copying)
+		 		Imgproc.threshold(M, A, 128, 255, Imgproc.THRESH_BINARY);
+		 		A.get(0,0,data);
+		 		
+
+		 		//Paint onto screen for debug
+		         Bitmap bmp;
+		         bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+		         Canvas  canvas = surfaceHolder.lockCanvas();
+		         if(canvas != null){
+		         	
+		         	canvas.drawBitmap(bmp, 0, 0, null); 
+		 }
+		     surfaceHolder.unlockCanvasAndPost(canvas);
+		     return null;
+		     }
+		     
+
+		     protected void onProgressUpdate(Integer... progress) {}
+
+			@Override
+			protected String doInBackground(byte[]... params) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+		     			
+		};
+ 		//Reset controls
+ 		orientationEventListener.disable();
+ 		findViewById(R.id.buttonTake).setEnabled(false);
+ 		//execute
+		ComputeSudoku.execute(data);
+		//release camera
+		camera.release();
+
+}
+		
+		
+		
+	
 	
 	public void takePhoto(View view){
-		camera.takePicture(null, this, null);
+		camera.takePicture(null, null, this);
 	}
 
 	@Override
