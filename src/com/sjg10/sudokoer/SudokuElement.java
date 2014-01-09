1b84c319ee98a1f4ecbf84f6da4b691bec6d262f
Yi Zhang <yi.zhang7210@gmail.com>
2014-01-09 18:16:59 +0000

    Class definition of SudokuElement.

--- src/com/sjg10/sudokoer/SudokuElement.java
+++ src/com/sjg10/sudokoer/SudokuElement.java
@@ -1,5 +1,35 @@
+/**
+ *  Sudokoer
+ *  Component SudokuGrid
+ *  (C) 2014 by Samuel Gonshaw (sjg10@imperial.ac.uk)
+ *  
+ *  Sudokoer is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Sudokoer is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Sudokoer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.sjg10.sudokoer;
 
 public class SudokuElement {
+	public int location[];
+	public int content;
+	public boolean isDefinite;
+	
+	/* Constructor */
+	public SudokuElement(int[] location, int content, boolean isDefinite){
+		this.location = location;
+		this.content = content;
+		this.isDefinite = isDefinite;
+	}
+	
 
 }
